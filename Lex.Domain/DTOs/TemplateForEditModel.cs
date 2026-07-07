ae10eb139d4c70dd8ed8ea02f72e165168774361
Bodya <bodyaforst@gmail.com>
2026-07-08 03:47:13 +0400

    Добавил страницу создания документа на основе шаблона

---
+++ Lex.Domain/DTOs/TemplateForEditModel.cs
@@ -0,0 +1,12 @@
+﻿using Lex.Domain.Enums;
+
+namespace Lex.Domain.DTOs;
+
+public class TemplateForEditModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string CurrentContent { get; set; } = string.Empty;
+    public DocumentType Type { get; set; }
+}
