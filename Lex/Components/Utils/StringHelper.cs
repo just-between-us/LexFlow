ec040fb9d56c62022abc19d0a24c8b9210d764ec
Bodya <bodyaforst@gmail.com>
2026-07-15 23:24:17 +0400

    StringHelper и Truncate для сокрашение строк

---
+++ Lex/Components/Utils/StringHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Lex.Components.Utils;
+
+public static class StringHelper
+{
+    public static string Truncate(string text, int reduce = 10)
+    {
+        if (string.IsNullOrEmpty(text)) return text;
+        return text.Length <= 10 ? text : text.Substring(0, reduce) + "...";
+    }
+}
