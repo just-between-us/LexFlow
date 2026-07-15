71ba1c79a8a6c426c43a4337e8e6e253b7cff2ce
Bodya <bodyaforst@gmail.com>
2026-07-15 19:05:32 +0400

    fix пакетов http контекста

--- Lex.Domain/Interfaces/IActiveChecklistService.cs
+++ Lex.Domain/Interfaces/IActiveChecklistService.cs
@@ -1,9 +1,15 @@
-﻿using Lex.Domain.Entities;
+﻿using Lex.Domain.DTOs;
 
 namespace Lex.Domain.Interfaces;
 
 public interface IActiveChecklistService
 {
-    Task<ActiveChecklist> CreateFromTemplateAsync(Guid templateId, Guid userId, CancellationToken cancellationToken = default);
-    Task<ActiveChecklist?> GetActiveWithItemsAsync(Guid activeChecklistId, CancellationToken cancellationToken = default);
+    Task<ActiveChecklistDto?> GetActiveForUserAsync(Guid? userId, Guid checklistId, CancellationToken ct = default);
+    Task<ActiveChecklistDto> StartNewAsync(Guid userId, Guid checklistId, Guid? clientOrganizationId = null,
+        CancellationToken ct = default);
+    Task<ActiveChecklistDto> RestoreAsync(Guid activeChecklistId, CancellationToken ct = default);
+    Task<ActiveChecklistDetailsDto?> GetDetailsAsync(Guid activeChecklistId, CancellationToken ct = default);
+    Task ToggleItemAsync(Guid activeChecklistItemId, bool isCompleted, CancellationToken ct = default);
+    Task UpdateNoteAsync(Guid activeChecklistItemId, string? note, CancellationToken ct = default);
+    Task DeleteAsync(Guid activeChecklistId, CancellationToken ct = default);
 }
