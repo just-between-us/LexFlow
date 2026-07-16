067365c24bd6dca9bd24c0e9209aa0c1dff19cdb
Bodya <bodyaforst@gmail.com>
2026-07-16 20:08:01 +0400

    Страничка профиля пользователя

---
+++ Lex.Domain/DTOs/ProfileEditModel.cs
@@ -0,0 +1,14 @@
+﻿using Lex.Domain.Enums;
+
+namespace Lex.Domain.DTOs;
+
+public class ProfileEditModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public UserActivityType ActivityType { get; set; }
+    public string? JobTitle { get; set; }
+    public string? CompanyName { get; set; }
+    public string? Region { get; set; }
+    public DateOnly? BirthDate { get; set; }
+}
---
+++ Lex/Components/Utils/DisplayHelper.cs
@@ -0,0 +1,18 @@
+﻿using Lex.Domain.Enums;
+
+namespace Lex.Components.Utils;
+
+public static class DisplayHelper
+{
+    public static string GetActivityTypeDisplay(UserActivityType type) => type switch
+    {
+        UserActivityType.Guest => "Гость",
+        UserActivityType.IndividualEntrepreneur => "ИП",
+        UserActivityType.OOO => "ООО",
+        UserActivityType.SelfEmployed => "Самозанятый",
+        UserActivityType.Startup => "Стартап",
+        UserActivityType.Freelancer => "Фрилансер",
+        UserActivityType.TeamMember => "Участник команды",
+        _ => "Прочее"
+    };
+}
