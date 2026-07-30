5a9095f1a1a105f273528e92a0f0ec61bafc050a
just-between-us <bodyaforst@gmail.com>
2026-07-30 23:47:00 +0400

    фикс утилки

--- Lex/Components/Utils/StringHelper.cs
+++ Lex/Components/Utils/StringHelper.cs
@@ -5,6 +5,7 @@ public static class StringHelper
     public static string Truncate(string text, int reduce = 10)
     {
         if (string.IsNullOrEmpty(text)) return text;
+        if  (text.Length < reduce) return text;
         return text.Length <= 10 ? text : text.Substring(0, reduce) + "...";
     }
 }
